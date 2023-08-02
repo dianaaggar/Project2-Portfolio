5d31990d2f7623c76138cbf606458d424e79a9fc
dianaaggar <57739072+dianaaggar@users.noreply.github.com>
2023-08-01 23:18:58 -0400

    added component about me

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+      <div className="a-left">
+        <h1 className='me'> About Me</h1>
+        <div className="a-card">
+          <img src="https://media.tenor.com/PP9v7VIs6R4AAAAd/scaler-create-impact.gif" alt="" className="a-image" />
+        </div>
+      </div>
+      <div className="a-right">
+        <p className='a-description'>University educated, highly motivated, Software Engineer experienced in hardware and software troubleshooting.
+A persistent and well-rounded tutor with great listening skills with a keen eye to detail. A critical thinker with
+the ability to stay focused and attentive while identifying logical relationships and patterns between ideas.
+Fluent in French,English, Arabic and Berber.
+
+Personable | Fast Learner | Problem Solver | Creative | Smart | Customer Service Driven
+
+Calm Demeanor | Leadership Qualities | Adaptable | Friendly
+Problem Solving | Tutor | Reliable | Strong work ethic
+Selenium, Java, JavaScript, Python, HTML,
+CSS, SQL, Manual Testing and Jira.</p>
+</div>
+      
+    </div>
+  )
+}
+
+export default About
